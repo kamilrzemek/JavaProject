4c5cef993cf10e8c92ee38e739bbc4ed32acbf8d
Kamil Rzemek <kamilrzemek@mail.com>
2018-04-24 23:11:13 +0200

    hehe

--- src/JavaProject.java
+++ src/JavaProject.java
@@ -2,5 +2,6 @@ public class JavaProject {
     public static void main (String [] args){
         System.out.println("Hej");
         //Dodalem komentarz z laptopa, dobry jestem nie?
+        //Dodalem komentarz z komputera
     }
 }
